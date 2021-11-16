21806e31a52afa347e2a8d9bf8e6b3d5288785e8
qndaa <qndaa@uns.ac.rs>
2021-11-16 19:55:28 +0100

    fetch tests

--- backend/api/src/tests/views.py
+++ backend/api/src/tests/views.py
@@ -1,10 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.mixins import ListModelMixin
 from .models import Test
 from .serializers import TestSerializer
 
 
-class TestViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+class TestViewSet(ModelViewSet, ListModelMixin):
+    ModelViewSet.permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,5 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
+export default () => {
 
-export default function () {
+    const tests = [];
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/v1/tests/').then((response) => {
+            console.log(response);
+            this.tests = response.data.results;
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [])
+
     return <div>HOME</div>;
 }
